8745a893cf5f1718e90391a5138583a963621192
futurodevthalles <133525710+futurodevthalles@users.noreply.github.com>
2023-05-27 19:35:40 -0300

    Update desafio.kt
    *a enumeração Nivel agora possui o valor AVANCADO em vez de DIFICIL.
    
    *O construtor da classe Usuario recebe um parâmetro nome para inicializar o nome do usuário.
    
    *O campo conteudos na classe Formacao foi alterado para uma lista mutável (MutableList<ConteudoEducacional>), permitindo a adição e remoção de conteúdos.
    
    *No método matricular da classe Formacao, o usuário é adicionado à lista inscritos e é exibida uma mensagem de confirmação.
    
    *Na função main(), foram criados dois objetos Usuario (João e Maria) e três objetos ConteudoEducacional. Em seguida, um objeto Formacao chamado "Kotlin Básico" é criado, os usuários são matriculados na formação e uma mensagem de confirmação é exibida.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,28 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, val nivel: Nivel, val conteudos: MutableList<ConteudoEducacional>) {
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} matriculado na formação $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin")
+    val conteudo2 = ConteudoEducacional("Classes e Objetos")
+    val conteudo3 = ConteudoEducacional("Collections")
+
+    val formacao = Formacao("Kotlin Básico", Nivel.BASICO, mutableListOf(conteudo1, conteudo2, conteudo3))
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
 }
